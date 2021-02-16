84ac2a1ad916d540cd67c886ddcba766aaed751e
Junior <reinaldo.diasjunior@gmail.com>
2021-02-16 15:24:25 -0300

    Adds material icons

--- src/components/Todo/index.tsx
+++ src/components/Todo/index.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
 import Checkbox from '@material-ui/core/Checkbox'
+import IconButton from '@material-ui/core/IconButton'
+import DeleteIcon from '@material-ui/icons/Delete'
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
+import EditIcon from '@material-ui/icons/Edit'
 
 interface Props {
   task: string
@@ -14,6 +18,11 @@ const Todo: React.FC<Props> = ({ task, completed }) => {
       <ListItem>
         <Checkbox checked={completed} />
         <ListItemText>{task}</ListItemText>
+        <ListItemSecondaryAction>
+          <IconButton>
+            <DeleteIcon />
+          </IconButton>
+        </ListItemSecondaryAction>
       </ListItem>
     </>
   )
