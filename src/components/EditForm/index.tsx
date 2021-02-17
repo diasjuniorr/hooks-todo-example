9d69b3aff0b3c910ed3fc57b14e3fd3dfac7c51f
Junior <reinaldo.diasjunior@gmail.com>
2021-02-17 16:58:57 -0300

    Turns FormEdit into controlled input

--- src/components/EditForm/index.tsx
+++ src/components/EditForm/index.tsx
@@ -1,4 +1,5 @@
 import React, { FormEvent } from 'react'
+import useInputState from '../../hooks/useInputState'
 import TextField from '@material-ui/core/TextField'
 
 interface Props {
@@ -9,15 +10,16 @@ interface Props {
 }
 
 const EditForm: React.FC<Props> = ({ todoId, task, updateTodo, toggle }) => {
+  const [value, setValue] = useInputState(task)
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    updateTodo(todoId, 'task')
+    updateTodo(todoId, value)
     toggle()
   }
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <TextField fullWidth />
+        <TextField fullWidth value={value} onChange={setValue} />
       </form>
     </>
   )
