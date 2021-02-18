a017ece41a1abe60e43351651705135a1e0ebdfa
Junior <reinaldo.diasjunior@gmail.com>
2021-02-17 22:01:05 -0300

    Improves styles on absense of todos

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Paper from '@material-ui/core/Paper'
 import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
 import Divider from '@material-ui/core/Divider'
+import SentimentVeryDissatisfiedOutlinedIcon from '@material-ui/icons/SentimentVeryDissatisfiedOutlined'
 
 import Todo from '../Todo'
 
@@ -25,30 +25,32 @@ const TodoList: React.FC<Props> = ({
   updateTodoStatus,
   updateTodo,
 }) => {
-  return (
-    <>
-      <Paper>
-        <List>
-          {todos.map((todo, index) => {
-            return (
-              <>
-                <Todo
-                  key={todo.id}
-                  id={todo.id}
-                  task={todo.task}
-                  completed={todo.completed}
-                  removeTodo={removeTodo}
-                  updateTodoStatus={updateTodoStatus}
-                  updateTodo={updateTodo}
-                />
-                {index < todos.length - 1 && <Divider />}
-              </>
-            )
-          })}
-        </List>
-      </Paper>
-    </>
-  )
+  if (todos.length)
+    return (
+      <>
+        <Paper>
+          <List>
+            {todos.map((todo, index) => {
+              return (
+                <>
+                  <Todo
+                    key={todo.id}
+                    id={todo.id}
+                    task={todo.task}
+                    completed={todo.completed}
+                    removeTodo={removeTodo}
+                    updateTodoStatus={updateTodoStatus}
+                    updateTodo={updateTodo}
+                  />
+                  {index < todos.length - 1 && <Divider />}
+                </>
+              )
+            })}
+          </List>
+        </Paper>
+      </>
+    )
+  return null
 }
 
 export default TodoList
